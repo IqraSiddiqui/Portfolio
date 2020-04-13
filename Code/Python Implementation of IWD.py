d3b3ebf3899ac4923128bbd2420caec52c74aa46
IqraSiddiqui <59932139+IqraSiddiqui@users.noreply.github.com>
2020-04-13 11:53:16 +0500

    Update Python Implementation of IWD.py

--- Code/Python Implementation of IWD.py
+++ Code/Python Implementation of IWD.py
@@ -23,29 +23,4 @@ def IWD(c,nc):
     
     
     
-=======
-#------------IMPLEMENTATION OF INTELLIGENT WATER DROP ALGORITHM---------------------#
-
-nc= #integer i.e. number of cities
-c= #list of the Cartesian coordinates (x,y) of cities
-
-def tspIWD(c,nc):
-    Niwd=nc
-    av=1000
-    bv=0.01
-    cv=1
-    asoil=1000
-    bsoil=0.01
-    csoil=1
-    iniSoil=1000
-    iniVel=100
-    T=[]
-    for i in range(Niwd):
-        visited=[]
-        Soil=0
-        Vel=iniVel
-        
-    
-    
-    
 >>>>>>> Stashed changes
