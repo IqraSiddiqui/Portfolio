22e747567e81090d7ed4ecb8b604deebe0ae76bd
Sara Intikhab <si04515@st.habib.edu.pk>
2020-05-04 03:24:25 +0500

    Update Python Implementation of IWD.py
    Comments added

--- Code/Python Implementation of IWD.py
+++ Code/Python Implementation of IWD.py
@@ -46,11 +46,11 @@ def initializeIWD(Niwd):
             
             val=[]
             val.append(0)
-            val.append(iniVel)
+            val.append(iniVel)          # velocity is set to Initial Velocity. All IWDs are set to have zero amount of soil. 
 
             visited=[]
-            visit=random.choice(graph.keys()) #Step 3
-            visited.append(visit) #Step 4
+            visit=random.choice(graph.keys()) #Step 3  (Spread the IWDs randomly on the nodes of the graph as their first visited nodes)
+            visited.append(visit) #Step 4   (Update the visited node list of each IWD to include the nodes just visited)
             val.append(visited)
 
             iwd[i]=val
@@ -90,26 +90,49 @@ def HUD(graph):
 graph=adjlist(N,E) 
 def IWD(graph):
     soil={} #soil on path from some node to another
-
+    """Initialisation of static parameters. The graph (N, E) of
+    the problem is given to the algorithm. The quality of the
+    total-best solution TTB is initially set to the worst value:
+    ( ) TB q T = −∞ . The maximum number of iterations
+    itermax is specified by the user. The iteration count
+    itercount is set to zero.
+    The number of water drops NIWD is set to a positive
+    integer value, which is usually set to the number of
+    nodes Nc of the graph.
+    For velocity updating, the parameters are 1 v a = ,
+    .01 v b = and 1 v c = . For soil updating, 1 s a = , .01 s b =
+    and 1 s c = . The local soil updating parameter ρn ,
+    which is a small positive number less than one, is set as 
+    ρn = . The global soil updating parameter ρ IWD ,
+    which is chosen from [0, 1], is set as 0.9 ρ IWD = .
+    Moreover, the initial soil on each path (edge) is denoted
+    by the constant InitSoil such that the soil of the path
+    between every two nodes i and j is set by
+    soil i j InitSoil (, ) = . The initial velocity of each IWD is
+    set to InitVel. Both parameters InitSoil and InitVel are
+    user selected and they should be tuned experimentally
+    for the application. Here, InitSoil = 10000 and
+    InitVel = 200. For the IWD-MKP, InitVel = 4 is used,
+    which is the same value used in Shah-Hosseini (2008)"""
     #Step 1
-    Ttb= -1000000000000000000000000000000
-    Niwd=len(graph)
+    Ttb= -1000000000000000000000000000000           #total best solution set to worst value
+    Niwd=len(graph)                                 #number of water drops
     
-    av=1
+    av=1                #velocity parameters a, b, c
     bv=0.01
     cv=1
     
-    asoil=1
+    asoil=1             #soil parameters a, b, c
     bsoil=0.01
     csoil=1
     
-    pn=0.9
-    piwd=0.9
+    pn=0.9          #local soil parameter is set to less than 1
+    piwd=0.9            #global soil parameter
     
-    iniSoil=10000
-    iniVel=200
+    iniSoil=10000       #initial soil
+    iniVel=200      #intial velocity 
 
-    itercount=0
+    itercount=0             #iteration count is set to zero
     itermax=1000
     
    for iwd in range(len(graph)):
@@ -118,6 +141,10 @@ def IWD(graph):
             soil[iwd][j]=iniSoil
 
     while itercount<itermax:
+        """Initialisation of dynamic parameters. Every IWD has a
+        visited node list ( ) V IWD c , which is initially empty:
+        V IWD c ( ) = { } . Each IWD’s velocity is set to InitVel.
+        All IWDs are set to have zero amount of soil."""
         iwd=initializeIWD(Niwd) #Step 2
         for i in range(Niwd):
             
